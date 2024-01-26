5a837f9a8ac196b92c1117437b18d769dbd91282
VishnudevP <vipoil04@gmail.com>
2024-01-25 20:40:35 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Students from './pages/Students'
+import Courses from './pages/Courses'
+import Results from './pages/Results'
+import './style.css'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/students' element={<Students/>}/>
+      <Route path='/courses' element={<Courses/>}/>
+      <Route path='/results' element={<Results/>}/>
+    </Routes>
+  )
+}
+
+export default App
