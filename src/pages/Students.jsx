b04a87a13735b55936c93f2fba863a36ecc494da
VishnudevP <vipoil04@gmail.com>
2024-01-26 21:15:31 -0500

    Finished Student Page
    Local storage added
    Delete button added

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Students.css'
 import StudentForm from '../components/StudentForm'
 import StudentTable from '../components/StudentTable'
 
 const Students = () => {
   const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    const storedStudents = localStorage.getItem('students')
+    if (storedStudents) {
+      setStudents(JSON.parse(storedStudents))
+    }
+  }, []);
+
   const addStudent = (newStudent) => {
-    setStudents([...students, newStudent]);
+    setStudents([...students, newStudent])
+    localStorage.setItem('students', JSON.stringify([...students, newStudent]))
   }
+
+  const handleDeleteAll = () => {
+    setStudents([]);
+    localStorage.removeItem('students');
+  };
 
   return (
     <section className="container">
         <h1>Students</h1>
         <StudentForm onSubmit={addStudent} />
         <StudentTable students={students} />
+        <button onClick={handleDeleteAll} className="delete-all">
+        Delete All Students
+        </button>
     </section>
   )
 }
